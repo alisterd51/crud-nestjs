84001109c1c9ba84ef145161a7a9c0f5ac018581
antoine clarman <antoinereims28@gmail.com>
2023-05-01 06:21:56 +0200

    creating documentation for 'open-food-facts' routes with swagger

--- src/open-food-facts/open-food-facts.controller.ts
+++ src/open-food-facts/open-food-facts.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { OpenFoodFactsService } from './open-food-facts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @Controller('open-food-facts')
 @ApiTags('open-food-facts')
@@ -8,87 +8,164 @@ export class OpenFoodFactsController {
   constructor(private readonly openFoodFactsService: OpenFoodFactsService) {}
 
   @Get('brands')
+  @ApiOperation({ summary: 'Returns all brands' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getBrands() {
     return this.openFoodFactsService.getBrands();
   }
 
-  @Get('product/:id')
-  getProduct(@Param('id') id: string) {
-    return this.openFoodFactsService.getProduct(+id);
+  @Get('product/:barcode')
+  @ApiOperation({ summary: 'Get product by barcode' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
+  getProduct(@Param('barcode', new ParseIntPipe()) barcode: number) {
+    return this.openFoodFactsService.getProduct(barcode);
   }
 
-  @Get('brand/:id')
-  getBrand(@Param('id') id: string) {
-    return this.openFoodFactsService.getBrand(id);
+  @Get('brand/:brandName')
+  @ApiOperation({ summary: 'Get all details of a specific brand' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
+  getBrand(@Param('brandName') brandName: string) {
+    return this.openFoodFactsService.getBrand(brandName);
   }
 
   @Get('languages')
+  @ApiOperation({ summary: 'Get all languages on the labels' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getLanguages() {
     return this.openFoodFactsService.getLanguages();
   }
 
   @Get('labels')
+  @ApiOperation({ summary: 'Get all Labels from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getLabels() {
     return this.openFoodFactsService.getLabels();
   }
 
   @Get('additives')
+  @ApiOperation({ summary: 'Get all additives from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getAdditives() {
     return this.openFoodFactsService.getAdditives();
   }
 
   @Get('allergens')
+  @ApiOperation({ summary: 'Get all allergens from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getAllergens() {
     return this.openFoodFactsService.getAllergens();
   }
 
   @Get('categories')
+  @ApiOperation({ summary: 'Get all categories from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getCategories() {
     return this.openFoodFactsService.getCategories();
   }
 
   @Get('countries')
+  @ApiOperation({ summary: 'Get all countries from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getCountries() {
     return this.openFoodFactsService.getCountries();
   }
 
   @Get('entry-dates')
+  @ApiOperation({ summary: 'Get all entry dates from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getEntryDates() {
     return this.openFoodFactsService.getEntryDates();
   }
 
   @Get('ingredients')
+  @ApiOperation({ summary: 'Get all ingredients from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getIngredients() {
     return this.openFoodFactsService.getIngredients();
   }
 
   @Get('packaging')
+  @ApiOperation({ summary: 'Get all packagings from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getPackagings() {
     return this.openFoodFactsService.getPackagings();
   }
 
   @Get('packager-codes')
+  @ApiOperation({ summary: 'Get packaging codes from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getPacakgingCodes() {
     return this.openFoodFactsService.getPacakgingCodes();
   }
 
   @Get('purchase-places')
+  @ApiOperation({ summary: 'Get all purchase places from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getPurchasePlaces() {
     return this.openFoodFactsService.getPurchasePlaces();
   }
 
   @Get('states')
+  @ApiOperation({ summary: 'Get all states from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getStates() {
     return this.openFoodFactsService.getStates();
   }
 
   @Get('stores')
+  @ApiOperation({ summary: 'Get all stores from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
   getStores() {
     return this.openFoodFactsService.getStores();
   }
 
-  @Get('code/:id')
-  getProductsByBarcodeBeginning(@Param('id') id: string) {
-    return this.openFoodFactsService.getProductsByBarcodeBeginning(id);
+  @Get('traces')
+  @ApiOperation({ summary: 'Get all trace types from the API' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
+  getTraces() {
+    return this.openFoodFactsService.getTraces();
+  }
+
+  @Get('code/:beginning')
+  @ApiOperation({ summary: 'Get all products beginning with the given barcode string' })
+  @ApiBearerAuth()
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({ description: 'Access token is missing or invalid' })
+  getProductsByBarcodeBeginning(@Param('beginning') beginning: string) {
+    return this.openFoodFactsService.getProductsByBarcodeBeginning(beginning);
   }
 }
--- src/open-food-facts/open-food-facts.service.ts
+++ src/open-food-facts/open-food-facts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { openFoodFactsDefaultOptions } from './constants';
 import { AxiosCacheInstance, setupCache } from 'axios-cache-interceptor';
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 
 @Injectable()
 export class OpenFoodFactsService {
@@ -19,101 +19,90 @@ export class OpenFoodFactsService {
     });
   }
 
-  getProduct(barcode: number) {
-    return this.axios.get(`${this.URL}/api/2/product/${barcode}.json`).then((product) => {
-      return product.data;
-    });
+  async getProduct(barcode: number) {
+    const product = await this.axios.get(`${this.URL}/api/2/product/${barcode}.json`);
+    return product.data;
   }
 
-  getBrand(brandName: string) {
-    return this.axios.get(`${this.URL}/brand/${brandName}.json`).then((brands) => {
-      return brands.data;
-    });
+  async getBrand(brandName: string) {
+    const brands = await this.axios.get(`${this.URL}/brand/${brandName}.json`);
+    return brands.data;
   }
 
-  getLanguages() {
-    return this.axios.get(`${this.URL}/languages.json`).then((languages) => {
-      return languages.data;
-    });
+  async getLanguages() {
+    const languages = await this.axios.get(`${this.URL}/languages.json`);
+    return languages.data;
   }
 
-  getLabels() {
-    return this.axios.get(`${this.URL}}/labels.json`).then((labels) => {
-      return labels.data;
-    });
+  async getLabels() {
+    const labels = await this.axios.get(`${this.URL}}/labels.json`);
+    return labels.data;
   }
 
-  getAdditives() {
-    return this.axios.get(`${this.URL}/additives.json`).then((additives) => {
-      return additives.data;
-    });
+  async getAdditives() {
+    const additives = await this.axios.get(`${this.URL}/additives.json`);
+    return additives.data;
   }
 
-  getAllergens() {
-    return this.axios.get(`${this.URL}/allergens.json`).then((allergens) => {
-      return allergens.data;
-    });
+  async getAllergens() {
+    const allergens = await this.axios.get(`${this.URL}/allergens.json`);
+    return allergens.data;
   }
 
-  getCategories() {
-    return this.axios.get(`${this.URL}/categories.json`).then((categories) => {
-      return categories.data;
-    });
+  async getCategories() {
+    const categories = await this.axios.get(`${this.URL}/categories.json`);
+    return categories.data;
   }
 
-  getCountries() {
-    return this.axios.get(`${this.URL}/countries.json`).then((countries) => {
-      return countries.data;
-    });
+  async getCountries() {
+    const countries = await this.axios.get(`${this.URL}/countries.json`);
+    return countries.data;
   }
 
-  getEntryDates() {
-    return this.axios.get(`${this.URL}/entry-dates.json`).then((entry_dates) => {
-      return entry_dates.data;
-    });
+  async getEntryDates() {
+    const entry_dates = await this.axios.get(`${this.URL}/entry-dates.json`);
+    return entry_dates.data;
   }
 
-  getIngredients() {
-    return this.axios.get(`${this.URL}/ingredients.json`).then((ingredients) => {
-      return ingredients.data;
-    });
+  async getIngredients() {
+    const ingredients = await this.axios.get(`${this.URL}/ingredients.json`);
+    return ingredients.data;
   }
 
-  getPackagings() {
-    return this.axios.get(`${this.URL}/packaging.json`).then((packaging) => {
-      return packaging.data;
-    });
+  async getPackagings() {
+    const packaging = await this.axios.get(`${this.URL}/packaging.json`);
+    return packaging.data;
   }
 
-  getPacakgingCodes() {
-    return this.axios.get(`${this.URL}/packager-codes.json`).then((packager_codes) => {
-      return packager_codes.data;
-    });
+  async getPacakgingCodes() {
+    const packager_codes = await this.axios.get(`${this.URL}/packager-codes.json`);
+    return packager_codes.data;
   }
 
-  getPurchasePlaces() {
-    return this.axios.get(`${this.URL}/purchase-places.json`).then((purchase_places) => {
-      return purchase_places.data;
-    });
+  async getPurchasePlaces() {
+    const purchase_places = await this.axios.get(`${this.URL}/purchase-places.json`);
+    return purchase_places.data;
   }
 
-  getStates() {
-    return this.axios.get(`${this.URL}/states.json`).then((states) => {
-      return states.data;
-    });
+  async getStates() {
+    const states = await this.axios.get(`${this.URL}/states.json`);
+    return states.data;
   }
 
-  getStores() {
-    return this.axios.get(`${this.URL}/stores.json`).then((stores) => {
-      return stores.data;
-    });
+  async getStores() {
+    const stores = await this.axios.get(`${this.URL}/stores.json`);
+    return stores.data;
   }
 
-  getProductsByBarcodeBeginning(beginning: string) {
+  async getTraces() {
+    const stores = await this.axios.get(`${this.URL}/traces.json`);
+    return stores.data;
+  }
+
+  async getProductsByBarcodeBeginning(beginning: string) {
     const fill = 'x'.repeat(13 - beginning.length);
     const barcode = beginning.concat(fill);
-    return this.axios.get(`${this.URL}/code/${barcode}.json`).then((products) => {
-      return products.data;
-    });
+    const products = await this.axios.get(`${this.URL}/code/${barcode}.json`);
+    return products.data;
   }
 }
